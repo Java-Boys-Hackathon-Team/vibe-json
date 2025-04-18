7a5a99b4d72198ce04c2113e0a09087f956fd95d
RustamKuramshin <leo.in.empty@gmail.com>
2025-04-19 02:41:33 +0300

    commit

--- src/test/java/ru/javaboys/vibejson/deserialization/WorkflowDeserializationTest.java
+++ src/test/java/ru/javaboys/vibejson/deserialization/WorkflowDeserializationTest.java
@@ -1,4 +1,4 @@
-package ru.javaboys.vibejson.deserializatio;
+package ru.javaboys.vibejson.deserialization;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
