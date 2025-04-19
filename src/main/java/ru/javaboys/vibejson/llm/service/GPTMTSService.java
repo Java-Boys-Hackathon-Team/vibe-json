adf699f5288ef6adc96d40879593c1279b3d8ac5
WerderRZ <rzaxel@gmail.com>
2025-04-19 16:10:17 +0300

    Add sending messages to MTS GPT

--- src/main/java/ru/javaboys/vibejson/llm/service/GPTMTSService.java
+++ src/main/java/ru/javaboys/vibejson/llm/service/GPTMTSService.java
@@ -3,15 +3,21 @@ package ru.javaboys.vibejson.llm.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.javaboys.vibejson.llm.client.GPTMTSClient;
+import ru.javaboys.vibejson.llm.dto.ChatCompletionRequestDTO;
+import ru.javaboys.vibejson.llm.dto.ChatCompletionResponseDTO;
 import ru.javaboys.vibejson.llm.dto.GPTModelsDTO;
 
 @Service
 @RequiredArgsConstructor
-public class GPTService {
+public class GPTMTSService {
 
     private final GPTMTSClient gptClient;
 
     public GPTModelsDTO getGPTModels() {
         return gptClient.listModels();
+    }
+
+    public ChatCompletionResponseDTO sendNewMessage(ChatCompletionRequestDTO gptModelsDTO) {
+        return gptClient.getAnswer(gptModelsDTO);
     }
 }
---
+++ src/test/java/ru/javaboys/vibejson/gpt_mts/GPTMTSServiceTest.java
@@ -0,0 +1,65 @@
+package ru.javaboys.vibejson.gpt_mts;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.javaboys.vibejson.llm.dto.ChatCompletionRequestDTO;
+import ru.javaboys.vibejson.llm.dto.ChatCompletionResponseDTO;
+import ru.javaboys.vibejson.llm.dto.GPTModelsDTO;
+import ru.javaboys.vibejson.llm.dto.MessageDTO;
+import ru.javaboys.vibejson.llm.service.GPTMTSService;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+public class GPTMTSServiceTest {
+
+    @Autowired
+    private GPTMTSService modelService;
+
+    @Test
+    @DisplayName("Получение списка моделей")
+    public void getListModels() {
+        GPTModelsDTO dto = modelService.getGPTModels();   // <‑‑ метод блокирующий
+        Assertions.assertNotNull(dto);
+    }
+
+    @Test
+    @DisplayName("Отправка нового сообщения")
+    public void sendFirstMessage() {
+        ChatCompletionRequestDTO req = new ChatCompletionRequestDTO();
+        req.setModel("llama-3.3-70b-instruct");
+        req.setTemperature(new BigDecimal(0));
+
+        List<MessageDTO> list = new ArrayList<>();
+        list.add(MessageDTO.builder().role("system").content("Ты помощник").build());
+        list.add(MessageDTO.builder().role("use").content("У меня машина Ferrari").build());
+        req.setMessages(list);
+
+        ChatCompletionResponseDTO response = modelService.sendNewMessage(req);
+        Assertions.assertNotNull(response);
+    }
+
+    @Test
+    @DisplayName("Отправка сообщения с историей")
+    public void sendMessagesWithHistory() {
+        ChatCompletionRequestDTO req = new ChatCompletionRequestDTO();
+        req.setModel("llama-3.3-70b-instruct");
+        req.setTemperature(new BigDecimal(0));
+
+        List<MessageDTO> list = new ArrayList<>();
+        list.add(MessageDTO.builder().role("system").content("Ты помощник").build());
+        list.add(MessageDTO.builder().role("use").content("У меня машина Ferrari").build());
+        list.add(MessageDTO.builder().role("assistant").content("Вау, это потрясающе! Ferrari - это иконический и престижный бренд, известный своими высокопроизводительными и стильными автомобилями. Какой модель Ferrari у вас есть? Это классическая модель, такая как 250 GT или Dino, или современная модель, такая как 488 GTB или F8 Tributo?").build());
+        list.add(MessageDTO.builder().role("use").content("Напомни, какая у меня машина?").build());
+        req.setMessages(list);
+
+        ChatCompletionResponseDTO response = modelService.sendNewMessage(req);
+        Assertions.assertNotNull(response);
+    }
+
+}
